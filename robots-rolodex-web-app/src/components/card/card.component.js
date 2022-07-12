756681579ddef30ada4f99ced586344e17b6e43d
averroesk <averroeskissi@gmail.com>
2022-07-12 16:40:28 +0200

    Added little background images

--- robots-rolodex-web-app/src/components/card/card.component.js
+++ robots-rolodex-web-app/src/components/card/card.component.js
@@ -2,7 +2,7 @@ import './card.styles.css';
 
 const Card = ({ robot: { id, name, email } }) => (
 	<div className='card-container'>
-		<img alt={`robot ${name}`} src={ `https://robohash.org/${id}?set=set1&size=180x180` } />
+		<img alt={`robot ${name}`} src={ `https://robohash.org/${id}?set=set1&bgset=bg1&size=180x180` } />
 		<h2>{name}</h2>
 		<p>{email}</p>
 	</div>
